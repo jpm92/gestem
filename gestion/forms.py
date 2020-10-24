1f91bcf18a10bda3ee5e27c3b58e2a7791535c1b
Jesús Peña <jesuspm92@gmail.com>
2020-10-24 23:15:08 +0200

    Añadidos modulos en blanco necesarios asi como TODOs mas importantes.

---
+++ gestion/forms.py
@@ -0,0 +1,8 @@
+""" Este modulo contiene los formularios necesarios en la app gestion. """
+
+
+# TODO: Formulario para Articulos (Nota)
+# TODO: Formulario para nuevo Producto
+# TODO: Formulario para articulos (Pedido) en modo autogestion
+# TODO: Formulario para asignar CPM
+# TODO: Formulario para 
--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# TODO: Añadir vista para Tablón
+# TODO: Añadir vista para tablón Secretario
+# TODO: Añadir vista para Añadir CPM (Secretario)
+# TODO: Añadir vista para Marcar pedido como Lanzado (Secretario)
+# TODO: Añadir vista para recepcionar pedido
+# TODO: Añadir vista para formulario de Productos
+# TODO: Añadir vista para registro total
+# TODO: Añadir vista para formulario de Notas
+# TODO: Añadir formulario para registro de Notas en modo autogestión
