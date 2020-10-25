96dbffea4f7e25d26625cd697f48f96bc5d7a362
Jesús Peña <jesuspm92@gmail.com>
2020-10-25 18:56:11 +0100

    Creados formularios para la anotacion de articulos en la pizarra

--- gestion/forms.py
+++ gestion/forms.py
@@ -1,13 +1,23 @@
 """ Este modulo contiene los formularios necesarios en la app gestion. """
 from django import forms
+from .models import Articulo, Nota
 
 
-class ArticuloForm(forms.Form):
-    pass  # TODO: Crear ArticuloForm
+class ArticuloForm(forms.ModelForm):
+    """ Este formulario se utiliza para agregar articulos a la pizarra. """
+
+    class Meta:
+        model = Articulo
+        fields = ['producto', 'unidades']
 
 
-class EntregaForm(forms.Form):
-    pass  # TODO: Crear EntregaForm
+class NotaForm(forms.ModelForm):
+    """ Este formulario se utiliza para crear una nota a la que enlazar
+    articulos al rellenar el formulario de anotación de articulos. """
+
+    class Meta:
+        model = Nota
+        fields = ['entrega']
     # direccion = forms.ForeignKey blablabla -> modelo Entrega
 # TODO: Formulario para nuevo Producto
 # TODO: Formulario para articulos (Pedido) en modo autogestion
