de2a7b15825f37e1f311f74ba9c5eb0e9b377e1d
Jesús Peña <jesuspm92@gmail.com>
2020-10-25 17:33:35 +0100

    Añadido gestion/urls.py con algunas direcciones.

---
+++ gestion/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('tablon/', views.Tablon.as_view(), name='tablon'),
+    path('secretaria/', views.Secretario.as_view(), name='secretaria'),
+    path('anotar/', views.NuevaNota, name='anotar'),
+]
