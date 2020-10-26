11baa0dfc95f9fb22a05e22893dc482985924cc6
Jesús Peña <jesuspm92@gmail.com>
2020-10-26 20:01:33 +0100

    Modificado BASE_DIR para que cuadre con la nueva localizacion de settings.py

--- gestem/settings/base.py
+++ gestem/settings/base.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -47,7 +48,7 @@ ROOT_URLCONF = 'gestem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
