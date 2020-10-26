ce6fa339485a471d98b14578744affe053853143
Jesús Peña <jesuspm92@gmail.com>
2020-10-26 11:52:16 +0100

    Implementado django-select2 (falta el bloque extrajs en cada plantilla)

--- gestem/settings/base.py
+++ gestem/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gestion',
+    'django_select2',
 ]
 
 MIDDLEWARE = [
--- gestem/urls.py
+++ gestem/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gestion.urls')),
+    path("select2/", include("django_select2.urls")),
 ]
 
 admin.site.site_header = _("Administración de TERSTEM")
