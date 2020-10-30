62ef5397b271d9e445fbebcb80eb5c0522bab0ca
Jesús Peña <jesuspm92@gmail.com>
2020-10-30 16:26:41 +0100

    Registro de los modelos en admin.

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
+from gestion.models import (
+    Producto,
+    Articulo,
+    Pedido,
+    CentroGasto,
+    Entrega,
+    Almacen,
+    Fabricante,
+    Distribuidor,
+    Nota
+)
 
 # Register your models here.
 # TODO: Registrar modelos en el panel de administración.
+
+admin.site.register(Producto)
+admin.site.register(Articulo)
+admin.site.register(Pedido)
+admin.site.register(CentroGasto)
+admin.site.register(Entrega)
+admin.site.register(Almacen)
+admin.site.register(Fabricante)
+admin.site.register(Distribuidor)
+admin.site.register(Nota)
