acd8204c4a3bae00b6ae90c3744ce0dfc3882981
Jesús Peña <jesuspm92@gmail.com>
2020-11-04 22:31:14 +0100

    Externalizado script para copiar texto con un click en tablas.

---
+++ gestion/static/gestion/js/copiable.js
@@ -0,0 +1,30 @@
+function indexInParent(node) {
+  var children = node.parentNode.childNodes;
+  var num = 0;
+  for (var i=0; i<children.length; i++) {
+    if (children[i]==node) return num+1;
+    if (children[i].nodeType==1) num++;
+  }
+  return -1;
+}
+
+function copy(event) {
+  var target = event.target || event.srcElement;
+  var copyText = target.innerText || target.textContent;
+  navigator.clipboard.writeText(copyText)
+
+  // Seleccionar la celda -> No me interesa por ahora.
+  // var range = document.createRange();
+  // range.selectNode(target);
+  // window.getSelection().removeAllRanges();
+  // window.getSelection().addRange(range);
+}
+
+var columnas = document.getElementsByClassName("copiable")
+
+for (var i=0; i < columnas.length; i++) {
+  var columna = columnas[i];
+  var index = indexInParent(columna);
+  document.querySelectorAll("td:nth-child("+index+")")
+  .forEach(elem => elem.addEventListener("click", copy))
+}
