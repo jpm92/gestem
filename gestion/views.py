6639e5f68bd5b7b7455e3ef045879a98f080a040
Jesús Peña <jesuspm92@gmail.com>
2020-11-04 22:32:23 +0100

    Arreglo visual

--- gestion/views.py
+++ gestion/views.py
@@ -48,7 +48,7 @@ class Tablon(generic.ListView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
+        # Add in a QuerySet
         usuario = self.request.user
         total = Articulo.objects.filter(nota__usuario=usuario)
         estads = {
