94246dc2415528ff33405a00ffbe71fb09dd516b
Jesús Peña <jesuspm92@gmail.com>
2020-11-15 21:16:38 +0100

    Añadido boton mágico para crear pedidos en admin y más implementaciones

--- gestion/admin.py
+++ gestion/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.urls import path
+from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
 from gestem.aux import keygen
 from gestion.models import (
@@ -22,17 +24,22 @@ from gestion.models import (
 
 
 class ProductoAdmin(ImportExportModelAdmin):
-    pass
+
+    list_display = ('nombre_fabricante', 'fabricante', 'referencia', 'formato',
+                    'distribuidor', 'categoria')
+    list_filter = ('categoria',)
 
 
 class ArticuloAdmin(ImportExportModelAdmin):
 
-    list_display = ('producto', 'unidades', 'nota', 'estado', 'pedido',)
+    list_display = ('fecha', 'usuario', 'producto', 'unidades', 'nota',
+                    'estado', 'pedido',)
     list_filter = ('estado',)
     list_display_links = ('producto',)
-    actions = ['asignar_pedido']
+    actions = ['crear_pedido']
+    change_list_template = "gestion/articulos_admin.html"
 
-    def asignar_pedido(self, request, queryset):
+    def crear_pedido(self, request, queryset):
         f = queryset.first()
         p = Pedido.objects.create(
             codigo=keygen(),
@@ -44,16 +51,39 @@ class ArticuloAdmin(ImportExportModelAdmin):
             a.estado = 'i'
             a.save()
 
-    asignar_pedido.short_description = _("Crear pedido")
+        self.message_user(request, _(f'¡Pedido {p.codigo} creado con éxito!'))
+        return redirect('admin:gestion_pedido_changelist')
+
+    crear_pedido.short_description = _("Crear pedido")
+
+    def fecha(self, obj):
+        return obj.nota.fecha
+
+    def usuario(self, obj):
+        return obj.nota.usuario
+
+    def magia(self, request):
+        """ Este método se ejecuta al pulsar el boton en admin. Itera sobre
+        todos los artículos pendientes y los autoincluye en pedidos nuevos. """
+        self.message_user(request, _('¡Pedidos creados con éxito!'))
+        return redirect('admin:gestion_pedido_changelist')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('clasificar/', self.magia),
+        ]
+        return my_urls + urls
 
 
 class ArticuloInline(admin.TabularInline):
     model = Articulo
+    extra = 0
 
 
 class PedidoAdmin(ImportExportModelAdmin):
     list_display = ('fecha_creacion', 'codigo', 'cpm', 'estado',
-                    'distribuidor', 'entrega', 'centro_gasto')
+                    'narticulos', 'distribuidor', 'entrega', 'centro_gasto')
     list_filter = ('estado',)
     list_display_links = ('codigo',)
     readonly_fields = ('fecha_creacion', 'fecha_cpm', 'fecha_confirmacion',
@@ -71,6 +101,11 @@ class PedidoAdmin(ImportExportModelAdmin):
     )
     inlines = (ArticuloInline,)
 
+    def narticulos(self, obj):
+        return obj.articulo_set.count()
+
+    narticulos.short_description = _("nº articulos")
+
 
 class ExtendidoInLine(admin.TabularInline):
     model = PerfilExtendido
@@ -81,7 +116,7 @@ class UserAdmin(BaseUserAdmin):
     inlines = (ExtendidoInLine,)
 
 
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Articulo, ArticuloAdmin)
 admin.site.register(Pedido, PedidoAdmin)
 admin.site.register(CentroGasto)
